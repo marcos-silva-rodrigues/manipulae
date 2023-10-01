fae1135a348fdaed29c8c19f6cb34a418491f200
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-10-01 12:56:50 -0300

    feat: initial endpoint

---
+++ manipulae/Controllers/VideoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace manipulae.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoController : ControllerBase
+    {
+
+        [HttpGet]
+        public Dictionary<string, string> HelloMessage()
+        {
+            var json = new Dictionary<string, string>();
+            json.Add("Message", "Hello Manipulae");
+            return json;
+        }
+    }
+}
